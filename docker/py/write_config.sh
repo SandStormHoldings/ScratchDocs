4a202f67c19c001d524c14ed123a6956e1b5d878
Guy Romm <guy@ezscratch.com>
2017-07-08 22:24:22 +0300

    container namespace separation.

--- docker/py/write_config.sh
+++ docker/py/write_config.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-echo "PG_DSN='dbname=tasks password=passw0rd user=tasks host=$PG_PORT_5432_TCP_ADDR'" > /home/tasks/config_local.py
-echo "COUCHDB_URI='http://"$COUCH_PORT_5984_TCP_ADDR":5984/'" >> /home/tasks/config_local.py
-echo "REDIS_BROKER='redis://"$REDIS_PORT_6379_TCP_ADDR":6379/'" >> /home/tasks/config_local.py
+# PREFIX="$1"
+# PREFIX="$(echo $str | awk '{print toupper($0)}')"
+PGHOST="$(eval echo "$"$PREFIX"_PG_PORT_5432_TCP_ADDR")"
+RDHOST="$(eval echo "$"$PREFIX"_REDIS_PORT_6379_TCP_ADDR")"
+echo "PG_DSN='dbname=tasks password=passw0rd user=tasks host="$PGHOST"'" > /home/tasks/config_local.py
+echo "REDIS_BROKER='redis://"$RDHOST":6379/'" >> /home/tasks/config_local.py
 echo "WEBAPP_FORCE_IDENTITY='default_user'" >> /home/tasks/config_local.py
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+NAME='tasks'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PWD=$(pwd)
 IDU=$(id -u)
@@ -13,30 +13,30 @@ function pgconnstr() {
 function wait_for() {
     HOST="$1"
     PORT="$2"
-    NAME="$3"
+    SNAME="$3"
     echo "about to wait on $HOST : $PORT"
     CNT=0
     while ! nc -w 1 -z $HOST $PORT; do
-	echo 'waiting for '$NAME' to be available on '$HOST':'$PORT' ('$CNT')'
+	echo 'waiting for '$SNAME' to be available on '$HOST':'$PORT' ('$CNT')'
 	sleep 1
 	CNT=$[$CNT+1]
 	if (( $CNT > 10 )) ; then
 	    return 1
 	    fi
     done
-    echo "# $HOST:$PORT ($NAME) is replying"
+    echo "# $HOST:$PORT ($SNAME) is replying"
     return 0
 }
 
 function storage_clean() {
     echo '# REMOVING STORAGE CONTAINERS' ||:
-    docker rm -f pg ||:
-    docker rm -f redis ||:
+    docker rm -f $NAME"_pg" ||:
+    docker rm -f $NAME"_redis" ||:
     echo '# REMOVING STORAGE DATA' ||:
     rm -rf docker/pg/data/* 
 }
 function app_clean() {
-    docker rm -f tasks_py tasks_celery ||: &&
+    docker rm -f $NAME"_py" $NAME"_celery" ||: &&
     find ./ -type f -iname '*pyc' -exec rm -rf {} \; &&
     rm -rf tmp/modules/*py    
 
@@ -52,39 +52,39 @@ function install_prerequisites() {
     }
 
 function build_app() {
-    echo "# BUILDING TASKS/PY"
-    docker build -t tasks/py .    
+    echo "# BUILDING "$NAME"/PY"
+    docker build -t $NAME"/py" .
 }
 
 function build_pg() {
     echo "# BUILDING PG"
-    cd $DIR"/docker/pg" && docker build -t tasks/pg . ; cd $DIR
+    cd $DIR"/docker/pg" && docker build -t $NAME"/pg" . ; cd $DIR
     }
 
 function envs_obtain() {
-    REDISHOST=$(docker inspect redis | jq '.[0].NetworkSettings.Networks.bridge.IPAddress' | sed 's/"//g')
+    REDISHOST=$(docker inspect $NAME"_redis" | jq '.[0].NetworkSettings.Networks.bridge.IPAddress' | sed 's/"//g')
     
     pgconnstr
     echo "REDISHOST=$REDISHOST"
 
     PW="passw0rd"
-    PGHOST=$(docker inspect pg | jq '.[0].NetworkSettings.Networks.bridge.IPAddress' | sed 's/"//g')
+    PGHOST=$(docker inspect $NAME"_pg" | jq '.[0].NetworkSettings.Networks.bridge.IPAddress' | sed 's/"//g')
     echo "PGHOST=$PGHOST  # $PGCONNSTR"
     
-    CELERYHOST=$(docker inspect tasks_celery | jq '.[0].NetworkSettings.Networks.bridge.IPAddress' | sed 's/"//g')
+    CELERYHOST=$(docker inspect $NAME"_celery" | jq '.[0].NetworkSettings.Networks.bridge.IPAddress' | sed 's/"//g')
     echo "CELERYHOST=$CELERYHOST"
     
-    PYHOST=$(docker inspect tasks_py | jq '.[0].NetworkSettings.Networks.bridge.IPAddress' | sed 's/"//g')
+    PYHOST=$(docker inspect $NAME"_py" | jq '.[0].NetworkSettings.Networks.bridge.IPAddress' | sed 's/"//g')
     echo "PYHOST=$PYHOST"
     }
 function storage_launch() {
-    echo '# RUNNING REDIS' &&
-    docker run -d --name=redis redis &&
+    echo '# RUNNING '$NAME'_REDIS' &&
+    docker run -d "--name="$NAME"_redis" redis &&
     echo '# RUNNING POSTGRESQL' &&     PW="passw0rd" #$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
-    docker run -d --env 'USERMAP_UID='$IDU --env 'DB_NAME=tasks' --env 'DB_USER=tasks' --env 'DB_PASS='$PW --name=pg -v $PWD"/docker/pg/data:/var/lib/postgresql" tasks/pg &&
+    docker run -d --env 'USERMAP_UID='$IDU --env 'DB_NAME=tasks' --env 'DB_USER=tasks' --env 'DB_PASS='$PW "--name="$NAME"_pg" -v $PWD"/docker/pg/data:/var/lib/postgresql" $NAME"/pg" &&
     wait_for $PGHOST 5432 "pg"
     echo "# INSTALLING PG EXTENSION" &&
-    docker exec -ti pg /extensions.sh &&
+    docker exec -ti $NAME"_pg" /extensions.sh &&
     envs_obtain &&
     wait_for $REDISHOST 6379 "redis"
     
@@ -114,20 +114,21 @@ function storage_details_print() {
 }
 
 function schema_save() {
-    docker exec -t pg pg_dump -U postgres -N pg_catalog -x --no-owner --schema-only tasks | grep -v 'plpgsql' > $DIR"/schema.sql" && \
-	docker exec -t pg pg_dump -U postgres -N pg_catalog -x --no-owner --data-only tasks --table tags >> schema.sql && \
-	docker exec -t pg pg_dump -U postgres -N pg_catalog -x --no-owner --data-only tasks --table participants >> schema.sql && \	
+    docker exec -t $NAME"_pg" pg_dump -U postgres -N pg_catalog -x --no-owner --schema-only tasks | grep -v 'plpgsql' > $DIR"/schema.sql" && \
+	docker exec -t $NAME"_pg" pg_dump -U postgres -N pg_catalog -x --no-owner --data-only tasks --table tags >> schema.sql && \
+	docker exec -t $NAME"_pg" pg_dump -U postgres -N pg_catalog -x --no-owner --data-only tasks --table participants >> schema.sql && \	
 	dos2unix $DIR"/schema.sql"
     
 }
 
 function storage_populate() {
+    NUPPER="$(echo $NAME | awk '{print toupper($0)}')"
     envs_obtain &&
     echo '# LOADING POSTGRESQL DUMP' &&
     wait_for $PGHOST 5432 "pg" &&
     pv schema.sql | pgconn &&
     echo '# WRITING CONFIG' &&
-    docker run -u $IDU -ti --link redis --link pg -v $PWD":/home/tasks" --entrypoint=/home/tasks/docker/py/write_config.sh tasks/py &&
+    docker run -u $IDU -ti --link $NAME"_redis" --link $NAME"_pg" -v $PWD":/home/tasks" --entrypoint=/home/tasks/docker/py/write_config.sh -e "PREFIX=$NUPPER" $NAME"/py" &&
     storage_details_print
 }
 function celery_cmd() {
@@ -136,7 +137,7 @@ function celery_cmd() {
     else
 	FL=""
     fi
-    docker run -u $IDU $FL --name=tasks_celery --link redis --link pg -v $PWD":/home/tasks" --entrypoint /home/tasks/docker/py/celery.sh tasks/py
+    docker run -u $IDU $FL "--name="$NAME"_celery" --link $NAME"_redis" --link $NAME"_pg" -v $PWD":/home/tasks" --entrypoint /home/tasks/docker/py/celery.sh $NAME"/py"
     }
 runserver_cmd() {
     if [[ "$1" == "" ]] ; then
@@ -144,13 +145,13 @@ runserver_cmd() {
     else
 	FL=""
     fi
-    docker run -u $IDU $FL --name=tasks_py --link redis --link pg -v $PWD":/home/tasks" --entrypoint /home/tasks/docker/py/runserver.sh  tasks/py
+    docker run -u $IDU $FL "--name="$NAME"_py" --link $NAME"_redis" --link $NAME"_pg" -v $PWD":/home/tasks" --entrypoint /home/tasks/docker/py/runserver.sh  $NAME"/py"
     }
 function launch_app() {
     
-    echo '# RUNNING tasks/celery '$1 &&
+    echo '# RUNNING '$NAME'/celery '$1 &&
 	celery_cmd &&
-	echo '# RUNNING tasks/py' &&
+	echo '# RUNNING '$NAME'/py' &&
 	runserver_cmd $1 &&
 	envs_obtain &&
 	wait_for $PYHOST 8090 "py" &&
@@ -178,31 +179,31 @@ function run_all() {
 }
 
 function start() {
-    docker start pg redis tasks_py tasks_celery
+    docker start $NAME"_pg" $NAME"_redis" $NAME"_py" $NAME"_celery"
 }
 
 function stop() {
-    docker stop pg redis tasks_py tasks_celery
+    docker stop $NAME"_pg" $NAME"_redis" $NAME"_py" $NAME"_celery"
     }
 function restart_all() {
-    docker restart pg redis tasks_py tasks_celery
+    docker restart $NAME"_pg" $NAME"_redis" $NAME"_py" $NAME"_celery"
 }
 
 function attach() {
-    docker attach tasks_py
+    docker attach $NAME"_py"
 }
 
 function shell() {
-    docker exec -ti tasks_py /bin/sh
+    docker exec -ti $NAME"_py" /bin/sh
 }
 
 function restart() { # app only
-    docker restart tasks_py
+    docker restart $NAME"_py"
 }
 function restart_hard_and_log() {
     app_clean &&
 	launch_app &&
-	docker attach tasks_py 2>&1 | tee tasks_py.log &
+	docker attach $NAME"_py" 2>&1 | tee tasks_py.log &
 }
 function restart_hard_and_attach() {
     app_clean &&
@@ -240,8 +241,16 @@ if [[ "$1" == "--help" || "$1" == "help" || "$1" == "-h" ]] ; then
     echo "3.c. launch_app"
     echo "#####################################################"
 elif [[ "$0" != "bash" && "$0" != "-bash" ]] ; then
+    if [[ "$1" != "" ]] ; then
+	NAME="$1"
+	fi
+    echo "# running setup.sh ALL. NAME=$NAME. you can also source this file and override NAME by providing it as the first arg."
+    sleep 5
     all
     #echo "ERROR ($0): you must source this script first. $ source setup.sh"
 else
-    echo "# setup.sh sourced. run -h for help."
+    if [[ "$1" != "" ]] ; then
+	NAME="$1"
+	fi
+    echo "# setup.sh sourced. NAME=$NAME run -h for help."
 fi
